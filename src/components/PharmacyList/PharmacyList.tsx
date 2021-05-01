1cd9f06017f93c23ce6a35d5b488b1919a8be3ff
Chris Hardie <chardie@lixar.com>
2021-05-01 18:23:17 -0400

    23 started consuming endpoint to show pharmacies availabilities

--- src/components/PharmacyList/PharmacyList.tsx
+++ src/components/PharmacyList/PharmacyList.tsx
@@ -19,44 +19,77 @@ export function PharmacyList() {
     },
   });
 
-  const pharmacyList: Pharmacy[] = [
-    {
-      id: "1",
-      pharmacyName: "Stittsville IDA",
-      booking: true,
-      address: "250 Stittsville Main St. Stittsville, ON K2S 1S9",
-      phone: "613-835-3881",
-      website: "https://idapharmacy.com/",
-      lastUpdated: "N/a",
-    },
-    {
-      id: "2",
-      pharmacyName: "Costco Kanata",
-      booking: false,
-      address: "770 Silver Seven Road Ottawa, ON K2V 0A1",
-      phone: " 613-270-5500",
-      website: " https://www.costcopharmacy.ca/appointment",
-      lastUpdated: "N/a",
-    },
-    {
-      id: "3",
-      pharmacyName: "Example Pharmacy 1",
-      booking: false,
-      address: "123 Road Ottawa, ON K2V 0A1",
-      phone: " 613-111-1111",
-      website: " https://www.website.ca/appointment",
-      lastUpdated: "N/a",
-    },
-    {
-      id: "4",
-      pharmacyName: "Example Pharmacy 2",
-      booking: false,
-      address: "123 Road Ottawa, ON K2V 0A1",
-      phone: " 613-111-1111",
-      website: " https://www.website.ca/appointment",
-      lastUpdated: "N/a",
-    },
-  ];
+  const pharmacyList: Pharmacy[] =
+    data?.map((pharmacy) => {
+      const addressSegments: string[] = [];
+
+      const { address } = pharmacy.location;
+      if (address) {
+        if (address.line1) {
+          addressSegments.push(address.line1);
+        }
+
+        if (address.line2) {
+          addressSegments.push(address.line2);
+        }
+
+        if (address.city) {
+          addressSegments.push(address.city);
+        }
+
+        addressSegments.push(address.province);
+        addressSegments.push(address.postcode);
+      }
+
+      return {
+        id: pharmacy.id,
+        pharmacyName: pharmacy.location.name,
+        booking: true,
+        address: addressSegments.join(" "),
+        lastUpdated: "N/A",
+        phone: pharmacy.location.phone || "",
+        website: pharmacy.location.url || "",
+      };
+    }) || [];
+
+  // const pharmacyList: Pharmacy[] = [
+  //   {
+  //     id: "1",
+  //     pharmacyName: "Stittsville IDA",
+  //     booking: true,
+  //     address: "250 Stittsville Main St. Stittsville, ON K2S 1S9",
+  //     phone: "613-835-3881",
+  //     website: "https://idapharmacy.com/",
+  //     lastUpdated: "N/a",
+  //   },
+  //   {
+  //     id: "2",
+  //     pharmacyName: "Costco Kanata",
+  //     booking: false,
+  //     address: "770 Silver Seven Road Ottawa, ON K2V 0A1",
+  //     phone: " 613-270-5500",
+  //     website: " https://www.costcopharmacy.ca/appointment",
+  //     lastUpdated: "N/a",
+  //   },
+  //   {
+  //     id: "3",
+  //     pharmacyName: "Example Pharmacy 1",
+  //     booking: false,
+  //     address: "123 Road Ottawa, ON K2V 0A1",
+  //     phone: " 613-111-1111",
+  //     website: " https://www.website.ca/appointment",
+  //     lastUpdated: "N/a",
+  //   },
+  //   {
+  //     id: "4",
+  //     pharmacyName: "Example Pharmacy 2",
+  //     booking: false,
+  //     address: "123 Road Ottawa, ON K2V 0A1",
+  //     phone: " 613-111-1111",
+  //     website: " https://www.website.ca/appointment",
+  //     lastUpdated: "N/a",
+  //   },
+  // ];
 
   return (
     <section>
@@ -66,7 +99,7 @@ export function PharmacyList() {
           })
         : null}
 
-      {data ? <div>{data}</div> : undefined}
+      {data ? <div>{JSON.stringify(data, undefined, 2)}</div> : undefined}
     </section>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./App";
 
+let promise: Promise<void> = Promise.resolve();
+
 /**
  * The following sarts our mock api server based on an env var
  */
 if (process.env.REACT_APP_MOCK_API === "true") {
-  import("./mocks/browser")
-    .then(({ worker }) => worker.start())
-    // Useless finally required as floating promises must be handled appropriately
-    .finally(() => undefined);
+  promise = import("./mocks/browser").then(async ({ worker }) => {
+    await worker.start();
+  });
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Using a promise structure to defer rendering until mock api kicks in as
+// top-level awaits are too modern
+promise
+  .then(() => {
+    ReactDOM.render(<App />, document.getElementById("root"));
+  })
+  // We'll add proper logging eventually...
+  // eslint-disable-next-line no-console
+  .catch((err) => console.log(err));
