735f01d94c337bb07b09a31e1506cee72b3464f3
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-05-04 17:02:37 -0400

    update project info

--- src/containers/CommunityProjects/CommunityProjectsInfo.tsx
+++ src/containers/CommunityProjects/CommunityProjectsInfo.tsx
@@ -1,8 +1,8 @@
 export const projInfo = [
   {
-    title: "ontariocovid-19.com",
-    description: "A mobile friendly dashboard for daily Ontario COVID-19",
-    link: "https://www.ontariocovid-19.com",
+    title: "Covid Vaccine Notifier",
+    description: "Automatic notifications for vaccine appointment availability",
+    link: "https://covidvaccines.club/welcome",
   },
   {
     title: "ontariocovid-19.com",
@@ -10,23 +10,25 @@ export const projInfo = [
     link: "https://www.ontariocovid-19.com",
   },
   {
-    title: "ontariocovid-19.com",
-    description: "A mobile friendly dashboard for daily Ontario COVID-19",
-    link: "https://www.ontariocovid-19.com",
+    title: "vaccine-gta.com",
+    description:
+      "Overview of vaccine appointment avaiability at Toronto vaccination sites",
+    link: "https://vaccine-gta.ca",
   },
   {
-    title: "ontariocovid-19.com",
-    description: "A mobile friendly dashboard for daily Ontario COVID-19",
-    link: "https://www.ontariocovid-19.com",
+    title: "vaccineupdates.ca",
+    description: "Search and filter VaxHuntersCan tweets",
+    link: "http://vaccineupdates.ca",
   },
   {
-    title: "ontariocovid-19.com",
-    description: "A mobile friendly dashboard for daily Ontario COVID-19",
-    link: "https://www.ontariocovid-19.com",
+    title: "vaxlocator.ca",
+    description: "Locate vaccination sites in your postal code",
+    link: "https://vaxlocator.ca",
   },
   {
-    title: "ontariocovid-19.com",
-    description: "A mobile friendly dashboard for daily Ontario COVID-19",
-    link: "https://www.ontariocovid-19.com",
+    title: "vaccine-peel.ca",
+    description:
+      "Overview of vaccine appointment avaiability at Toronto vaccination sites",
+    link: "https://www.vaccine-peel.ca",
   },
 ];
