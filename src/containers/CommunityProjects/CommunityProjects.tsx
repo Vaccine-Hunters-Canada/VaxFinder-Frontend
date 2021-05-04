2240bccb8b2b056e59857a39320a88e34e69bfea
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-05-04 19:50:53 -0400

    Map over projInfo to render cards

--- src/containers/CommunityProjects/CommunityProjects.tsx
+++ src/containers/CommunityProjects/CommunityProjects.tsx
@@ -1,34 +1,22 @@
-import { Page, Card } from "@shopify/polaris";
+import { Page } from "@shopify/polaris";
 import React from "react";
 import { projInfo } from "./CommunityProjectsInfo";
+import { CommunityProjectCard } from "./CommunityProjectCard";
 import "./CommunityProjectsStyles.css";
 
 export function CommunityProjects() {
-  const infoCards: JSX.Element[] = [];
-  projInfo.forEach((element) => {
-    const card = (
-      <div className="singleCardWrapper">
-        <Card
-          title={element.title}
-          sectioned
-          footerActionAlignment="left"
-          primaryFooterAction={{
-            content: "View Project",
-            external: true,
-            url: String(element.link),
-          }}
-        >
-          <p>{element.description}</p>
-        </Card>
-      </div>
-    );
-    infoCards.push(card);
-  });
-
   return (
     <Page title="Community Projects">
       <p>Explore more COVID-19 projects built by people from across Canada!</p>
-      <div className="cardFlexBox">{infoCards}</div>
+      <div className="cardFlexBox">
+        {projInfo.map((project) => (
+          <CommunityProjectCard
+            title={project.title}
+            link={project.link}
+            description={project.description}
+          />
+        ))}
+      </div>
     </Page>
   );
 }
