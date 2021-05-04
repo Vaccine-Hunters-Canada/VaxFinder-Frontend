397038e60984e7421c86c75672a8e94558d7a4f3
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-05-04 11:51:03 -0400

    Completed initial layout and styling

---
+++ src/components/CommunityProjects/CommunityProjects.tsx
@@ -0,0 +1,81 @@
+import {
+  Layout,
+  Page,
+  Banner,
+  Form,
+  FormLayout,
+  TextField,
+  Button,
+  Badge,
+  Card,
+} from "@shopify/polaris";
+import React, { useState } from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+import "./CommunityProjectsStyles.css";
+
+export function CommunityProjects() {
+  return (
+    <>
+      <Layout>
+        <Layout.Section>
+          <Page title="Community Projects">
+            <p>
+              Explore more COVID-19 projects built by people from across Canada!
+            </p>
+            <div className="cardFlexBox">
+              <div className="singleCardWrapper">
+                <Card title="ontariocovid-19.com" sectioned>
+                  <p>
+                    A mobile friendly dashboard for daily Ontario COVID-19
+                    updates.
+                  </p>
+                </Card>
+              </div>
+              <div className="singleCardWrapper">
+                <Card title="ontariocovid-19.com" sectioned>
+                  <p>
+                    A mobile friendly dashboard for daily Ontario COVID-19
+                    updates.
+                  </p>
+                </Card>
+              </div>
+              <div className="singleCardWrapper">
+                <Card title="ontariocovid-19.com" sectioned>
+                  <p>
+                    A mobile friendly dashboard for daily Ontario COVID-19
+                    updates.
+                  </p>
+                </Card>
+              </div>
+              <div className="singleCardWrapper">
+                <Card title="ontariocovid-19.com" sectioned>
+                  <p>
+                    A mobile friendly dashboard for daily Ontario COVID-19
+                    updates.
+                  </p>
+                </Card>
+              </div>
+              <div className="singleCardWrapper">
+                <Card title="ontariocovid-19.com" sectioned>
+                  <p>
+                    A mobile friendly dashboard for daily Ontario COVID-19
+                    updates.
+                  </p>
+                </Card>
+              </div>
+            </div>
+          </Page>
+        </Layout.Section>
+        <Layout.Section secondary>
+          <Page>
+            <TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="VaxHuntersCan"
+              options={{ height: "85vh" }}
+            />
+          </Page>
+        </Layout.Section>
+      </Layout>
+    </>
+  );
+}
---
+++ src/components/CommunityProjects/index.ts
@@ -0,0 +1,1 @@
+export { CommunityProjects } from "./CommunityProjects";
