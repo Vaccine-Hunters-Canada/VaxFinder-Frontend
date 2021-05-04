64eed7122dd2dc388fcf591d8c75016d07ad0288
Scott Evans <scott.evans@live.ca>
2021-05-04 17:56:49 -0400

    Order by available-first (#44)
    * pharmacies sorted available-first
    
    * order by available tests
    
    * order by available tests
    
    * order by available tests
    
    * order by available tests
    
    * order by available tests
    
    * order by available tests
    
    * order by available tests
    
    * order by available tests
    
    Co-authored-by: Scott McKinley Evans <scottmckinleyevans@Scotts-MacBook-Air.local>

--- src/containers/PharmacyList/PharmacyList.test.tsx
+++ src/containers/PharmacyList/PharmacyList.test.tsx
@@ -1,8 +1,7 @@
 import { rest } from "msw";
 import React from "react";
-import { VaccineAvailabilityExpandedResponse } from "../../apiClient";
 import { server } from "../../mocks/server";
-import { render, screen } from "../../testUtils";
+import { render, screen, within } from "../../testUtils";
 import { PharmacyList } from "./PharmacyList";
 import { format } from "date-fns-tz";
 
@@ -12,71 +11,69 @@ const formattedDate = format(
 );
 
 describe("PharmacyList", () => {
-  test("Should show returned pharmacy details with available appointments", async () => {
+  test("Should show all returned pharmacy details with appointment details", async () => {
     render(<PharmacyList postalCode="k2s 1s9" />);
 
-    await screen.findByRole("heading", { name: /stittsville ida/i });
-    await screen.findByText(/appointments available/i);
-    await screen.findByText(`as of ${formattedDate}`);
-    await screen.findByText(
+    await screen.findByRole("heading", {
+      name: /location1 with availability/i,
+    });
+    await screen.findByRole("heading", {
+      name: /location2 with availability/i,
+    });
+    await screen.findByRole("heading", {
+      name: /location1 with no availability/i,
+    });
+    await screen.findByRole("heading", {
+      name: /location2 with no availability/i,
+    });
+
+    const available = await screen.findAllByText(/appointments available/i);
+    expect(available.length).toBe(2);
+
+    const unavailable = await screen.findAllByText(
+      /appointments not available/i,
+    );
+    expect(unavailable.length).toBe(2);
+
+    const dates = await screen.findAllByText(`as of ${formattedDate}`);
+    expect(dates.length).toBe(4);
+
+    const addresses = await screen.findAllByText(
       /250 stittsville main st stittsville ontario k2s 1s9/i,
     );
-    await screen.findByText(/\(613\) 555-5555/i);
-    await screen.findByRole("link", { name: /https:\/\/idapharmacy\.com\//i });
+    expect(addresses.length).toBe(4);
+
+    const phones = await screen.findAllByText(/\(613\) 555-5555/i);
+    expect(phones.length).toBe(4);
+
+    const links = await screen.findAllByRole("link", {
+      name: /https:\/\/idapharmacy\.com\//i,
+    });
+    expect(links.length).toBe(4);
   });
 
-  test("Should show returned pharmacy details with no available appointments", async () => {
-    const response: VaccineAvailabilityExpandedResponse[] = [
-      {
-        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        numberAvailable: 0,
-        numberTotal: 0,
-        date: "2021-05-01",
-        vaccine: 1,
-        inputType: 1,
-        tags: "high-risk",
-        created_at: "2021-05-01T21:07:28.232Z",
-        location: {
-          id: 0,
-          name: "Stittsville IDA",
-          phone: "(613) 555-5555",
-          notes: "",
-          active: 1,
-          postcode: "K2K 2N6",
-          url: "https://idapharmacy.com/",
-          tags: "IDA",
-          created_at: "2021-05-01T21:07:28.232Z",
-          organization: 0,
-          address: {
-            id: 123456,
-            line1: "250 Stittsville Main St",
-            line2: "",
-            city: "Stittsville",
-            province: "Ontario",
-            postcode: "K2S 1S9",
-            created_at: "2021-05-01T21:07:28.232Z",
-          },
-        },
-      },
-    ];
-
-    server.use(
-      rest.get(
-        `${process.env.REACT_APP_API_URL!}/api/v1/vaccine-availability`,
-        async (req, res, ctx) => res(ctx.json(response)),
-      ),
-    );
-
+  test("Should show pharmacies with available appointments first", async () => {
     render(<PharmacyList postalCode="k2s 1s9" />);
+    const pharmacyList = await screen.findByLabelText(/pharmacy-list/i);
 
-    await screen.findByRole("heading", { name: /stittsville ida/i });
-    await screen.findByText(/appointments not available/i);
-    await screen.findByText(`as of ${formattedDate}`);
-    await screen.findByText(
-      /250 stittsville main st stittsville ontario k2s 1s9/i,
+    // pharmacyList.childNodes[0] is the eligibility notice
+
+    /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+    /* eslint-disable @typescript-eslint/no-unsafe-call */
+    await within(pharmacyList.childNodes[1]).findByText(
+      /appointments available/i,
     );
-    await screen.findByText(/\(613\) 555-5555/i);
-    await screen.findByRole("link", { name: /https:\/\/idapharmacy\.com\//i });
+    await within(pharmacyList.childNodes[2]).findByText(
+      /appointments available/i,
+    );
+    await within(pharmacyList.childNodes[3]).findByText(
+      /appointments not available/i,
+    );
+    await within(pharmacyList.childNodes[4]).findByText(
+      /appointments not available/i,
+    );
+    /* eslint-enable @typescript-eslint/no-unsafe-member-access */
+    /* eslint-enable @typescript-eslint/no-unsafe-call */
   });
 
   test("Should show error if pharmacy request fails", async () => {
--- src/containers/PharmacyList/PharmacyList.tsx
+++ src/containers/PharmacyList/PharmacyList.tsx
@@ -54,10 +54,10 @@ export function PharmacyList(props: Props) {
     );
   }
 
-  let pharmacyList: PharmacyProps[] = [];
+  let pharmacyListUnsorted: PharmacyProps[] = [];
 
   if (data) {
-    pharmacyList = data.map((pharmacy) => {
+    pharmacyListUnsorted = data.map((pharmacy) => {
       const addressSegments: string[] = [];
 
       const { address } = pharmacy.location;
@@ -91,8 +91,18 @@ export function PharmacyList(props: Props) {
     });
   }
 
+  const pharmacyList = pharmacyListUnsorted
+    .filter((pharmacy) => {
+      return pharmacy.booking;
+    })
+    .concat(
+      pharmacyListUnsorted.filter((pharmacy) => {
+        return !pharmacy.booking;
+      }),
+    );
+
   return (
-    <section>
+    <section aria-label="pharmacy-list">
       {shouldShowBanner ? (
         <EligibilityBanner onDismiss={() => setShouldShowBanner(false)} />
       ) : null}
