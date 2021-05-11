76d1be4e6bdddb1644d1a46dbb13f5baca27251b
Scott McKinley Evans <scottmckinleyevans@Scotts-MacBook-Air.local>
2021-05-11 19:26:58 -0400

    motck data with requirements

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -40,18 +40,20 @@ const vaccineLocationOneAvailability = {
       timeslots: [],
       requirements: [
         {
-          id: "0d114a64-9c09-48a3-927f-8b2d46a21feb",
+          id: "b0e80a42-7b7a-4259-a66e-f6c5664d66a7",
           vaccineAvailability: "dce1a460-2868-407d-8b74-04808cf1d55d",
-          requirement: 1,
+          requirement: 5,
           active: true,
-          createdAt: "2021-05-02T05:40:44.407000",
+          name: "Indigenous Adults",
+          description: "Indigenous Adults",
+          createdAt: "2021-05-02T05:40:43.243000+00:00",
         },
       ],
     },
   ],
 };
 
-const vaccineLocationMultipleAvailabilities = {
+const vaccineLocationSeperateDayAvailabilities = {
   name: "St. James Town Wellesley Community Centre (WCC)",
   phone: "1 888 385 1911",
   notes: undefined,
@@ -93,11 +95,13 @@ const vaccineLocationMultipleAvailabilities = {
       timeslots: [],
       requirements: [
         {
-          id: "ec5c8566-aa4c-4d19-8d27-b8b19b0259e6",
+          id: "b0e80a42-7b7a-4259-a66e-f6c5664d66a7",
           vaccineAvailability: "94bd5a16-0ae4-4d8e-bcf2-03f1898f9d71",
-          requirement: 4,
+          requirement: 5,
           active: true,
-          createdAt: "2021-05-02T05:40:47.683000",
+          name: "Indigenous Adults",
+          description: "Indigenous Adults",
+          createdAt: "2021-05-02T05:40:43.243000+00:00",
         },
       ],
     },
@@ -114,11 +118,14 @@ const vaccineLocationMultipleAvailabilities = {
       timeslots: [],
       requirements: [
         {
-          id: "19ae9c2f-89be-4f7a-81ae-d4f035251882",
+          id: "ab896245-7a11-49e0-a343-5e67f14bc271",
           vaccineAvailability: "3133fffb-a875-4184-957f-e69f761fd83c",
           requirement: 2,
           active: true,
-          createdAt: "2021-05-02T05:40:49.310000",
+          name: "Medical Conditions",
+          description:
+            "Organ Transplant Recipients and Individuals Receiving Active Cancer Treatment",
+          createdAt: "2021-05-02T05:41:01.310000+00:00",
         },
       ],
     },
@@ -135,11 +142,14 @@ const vaccineLocationMultipleAvailabilities = {
       timeslots: [],
       requirements: [
         {
-          id: "c9b54d5a-eaaf-4611-a7fa-035d39e80b41",
+          id: "ab896245-7a11-49e0-a343-5e67f14bc271",
           vaccineAvailability: "a5585900-1201-4364-a845-fe87003ff0dc",
-          requirement: 3,
+          requirement: 2,
           active: true,
-          createdAt: "2021-05-02T05:40:54.240000",
+          name: "Medical Conditions",
+          description:
+            "Organ Transplant Recipients and Individuals Receiving Active Cancer Treatment",
+          createdAt: "2021-05-02T05:41:01.310000+00:00",
         },
       ],
     },
@@ -309,19 +319,98 @@ const vaccineLocationsMixedAvailabilities = {
   ],
 };
 
+const vaccineLocatioSameDayMultipleAvailabilities = {
+  name: "West Park Healthcare Centre",
+  phone: "1-888-385-1910",
+  notes: undefined,
+  active: 1,
+  postcode: "M6M2J5",
+  url: "http://www.wpcovidclinic.org/",
+  tags: undefined,
+  id: 6,
+  organization: {
+    fullName: "Verto",
+    shortName: "Verto",
+    description: "Toronto Hospitals",
+    url: "https://vaccineto.ca/api/slots",
+    id: 1,
+    createdAt: "2021-05-02T03:20:59.077000+00:00",
+  },
+  address: {
+    line1: "83 Buttonwood Avenue",
+    line2: undefined,
+    city: "Toronto",
+    province: "Ontario",
+    postcode: "M6M2J5",
+    id: 9,
+    createdAt: "2021-05-03T00:03:07.590000+00:00",
+  },
+  createdAt: "2021-05-02T03:25:51.573000+00:00",
+  vaccineAvailabilities: [
+    {
+      numberAvailable: 5,
+      numberTotal: 5,
+      vaccine: 1,
+      inputType: 1,
+      tags: undefined,
+      id: "dce1a460-2868-407d-8b74-04808cf1d55d",
+      location: 1,
+      createdAt: "2021-05-06T00:00:03.120000",
+      date: "2021-05-08T04:00:00+00:00",
+      timeslots: [],
+      requirements: [
+        {
+          id: "b0e80a42-7b7a-4259-a66e-f6c5664d66a7",
+          vaccineAvailability: "dce1a460-2868-407d-8b74-04808cf1d55d",
+          requirement: 5,
+          active: true,
+          name: "Indigenous Adults",
+          description: "Indigenous Adults",
+          createdAt: "2021-05-02T05:40:43.243000+00:00",
+        },
+      ],
+    },
+    {
+      numberAvailable: 3,
+      numberTotal: 3,
+      vaccine: 1,
+      inputType: 1,
+      tags: undefined,
+      id: "f5f5e8a8-0dbd-4c0e-927a-22890b9b6609",
+      location: 4,
+      createdAt: "2021-05-06T00:00:14.957000+00:00",
+      date: "2021-05-08T04:00:00+00:00",
+      timeslots: [],
+      requirements: [
+        {
+          id: "8de54112-4e5c-476a-bff1-e005d0874e34",
+          vaccineAvailability: "f5f5e8a8-0dbd-4c0e-927a-22890b9b6609",
+          requirement: 6,
+          active: true,
+          name: "Caregivers",
+          description: "Caregivers",
+          createdAt: "2021-05-02T05:41:07.853000+00:00",
+        },
+      ],
+    },
+  ],
+};
+
 const vaccineLocationResponses = [
   vaccineLocationOneAvailability,
-  vaccineLocationMultipleAvailabilities,
+  vaccineLocationSeperateDayAvailabilities,
   vaccineLocationNoAvailabilities,
   vaccineLocationsZeroValueAvailability,
   vaccineLocationsMixedAvailabilities,
+  vaccineLocatioSameDayMultipleAvailabilities,
 ];
 
 export {
   vaccineLocationOneAvailability,
-  vaccineLocationMultipleAvailabilities,
+  vaccineLocationSeperateDayAvailabilities,
   vaccineLocationNoAvailabilities,
   vaccineLocationsZeroValueAvailability,
   vaccineLocationsMixedAvailabilities,
   vaccineLocationResponses,
+  vaccineLocatioSameDayMultipleAvailabilities,
 };
