28675ecb8f09c5cd90fc45f9f782600d006773ee
Andrew MacKinnon <andrewpmk@gmail.com>
2021-05-16 15:14:05 -0400

    Switch to eager loading for resource files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,19 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { format as date_fnz_tz_format } from "date-fns-tz";
-import enCA from "date-fns/locale/en-CA";
-import frCA from "date-fns/locale/fr-CA";
-// don't want to use this?
-// have a look at the Quick start guide
-// for passing in lng and translations on init
+import { format as date_fns_tz_format } from "date-fns-tz";
+import { enCA as date_fns_enCA, frCA as date_fns_frCA } from "date-fns/locale";
+import enCA from "./locales/en/translation.json";
+import frCA from "./locales/fr/translation.json";
 
 i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -22,17 +16,20 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en",
+    fallbackLng: "en-CA",
     debug: false,
-
+    resources: {
+      "en-CA": { translation: enCA },
+      "fr-CA": { translation: frCA },
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
       format: function format(value, fmt, lng) {
         if (value instanceof Date && fmt !== undefined) {
           if (lng?.substring(0, 2) === "fr") {
-            return date_fnz_tz_format(value, fmt, { locale: frCA });
+            return date_fns_tz_format(value, fmt, { locale: date_fns_frCA });
           }
-          return date_fnz_tz_format(value, fmt, { locale: enCA });
+          return date_fns_tz_format(value, fmt, { locale: date_fns_enCA });
         }
         return value;
       },
--- src/i18nfortests.ts
+++ src/i18nfortests.ts
@@ -1,33 +1,34 @@
 /* eslint-disable import/no-default-export */
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-fs-backend";
-import { format as date_fnz_tz_format } from "date-fns-tz";
-import { enCA, frCA } from "date-fns/locale";
+import { format as date_fns_tz_format } from "date-fns-tz";
+import { enCA as date_fns_enCA, frCA as date_fns_frCA } from "date-fns/locale";
+import enCA from "./locales/en/translation.json";
+import frCA from "./locales/fr/translation.json";
 
 i18n
   .use(initReactI18next)
-  .use(Backend)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    lng: "en",
-    fallbackLng: "en",
+    lng: "en-CA",
+    fallbackLng: "en-CA",
+    debug: true,
+    resources: {
+      "en-CA": { translation: enCA },
+      "fr-CA": { translation: frCA },
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-      // eslint-disable-next-line func-names
-      format: function (value, format, lng) {
-        if (value instanceof Date && format !== undefined) {
-          if (lng !== undefined && lng.substring(0, 2) === "fr") {
-            return date_fnz_tz_format(value, format, { locale: frCA });
+      format: function format(value, fmt, lng) {
+        if (value instanceof Date && fmt !== undefined) {
+          if (lng?.substring(0, 2) === "fr") {
+            return date_fns_tz_format(value, fmt, { locale: date_fns_frCA });
           }
-          return date_fnz_tz_format(value, format, { locale: enCA });
+          return date_fns_tz_format(value, fmt, { locale: date_fns_enCA });
         }
         return value;
       },
-    },
-    backend: {
-      loadPath: "./public/locales/en/translation.json",
     },
   });
 
