6533e1bf481c30defd2000e608ce9d61d545dbaa
Chris Hardie <chrishardie@outlook.com>
2021-05-29 20:09:26 -0400

    100 fixed private route component

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Redirect, Route, RouteProps } from "react-router-dom";
 import { userService } from "../../services/userService";
 
-export function PrivateRoute(props: RouteProps) {
-  if (!userService.checkIsAuthenticated()) {
-    return (
-      <Redirect
-        to={{
-          pathname: "/login",
-        }}
-      />
-    );
-  }
-  return <Route {...props} />;
-}
+export const PrivateRoute = ({ render, ...rest }: RouteProps) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        userService.checkIsAuthenticated() ? (
+          render && render(props)
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
