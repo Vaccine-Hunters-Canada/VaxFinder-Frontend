e3ee2af3cfe198e77a847508423a4cea9a4cf4dd
Chris Hardie <chrishardie@outlook.com>
2021-06-08 16:52:59 -0400

    121 lint

--- src/containers/RemoveAppointments/RemoveAppointments.tsx
+++ src/containers/RemoveAppointments/RemoveAppointments.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import queryString from "query-string";
 import { Redirect, useLocation } from "react-router-dom";
 import {
