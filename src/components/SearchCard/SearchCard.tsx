e7497abfb84d13608b443cd35f037f539d8fbbb2
ericherscovich <71022385+ericherscovich@users.noreply.github.com>
2021-08-05 16:04:52 -0400

    Fix logo (#155)
    * Switch webhook endpoint
    
    * added reporting tag
    
    * Update website styling and form
    
    * bump up logo
    
    * Add more informational and design changes
    
    * switch image link to local
    
    * remove top left logo, make image clickable
    
    * fix logo

--- src/components/SearchCard/SearchCard.tsx
+++ src/components/SearchCard/SearchCard.tsx
@@ -25,7 +25,9 @@ export function SearchCard() {
 
   return (
     <section aria-label="search" style={{ marginBottom: "2rem" }}>
-      <img src={image} alt="FYI Logo" width="100%" />
+      <a href="/">
+        <img src={image} alt="FYI Logo" width="100%" />
+      </a>
       <Card>
         <Card.Section>
           <Form onSubmit={handleSubmit}>
